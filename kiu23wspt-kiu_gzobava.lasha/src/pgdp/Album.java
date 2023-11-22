d7af72fe01dc7965b05f23da7529ed1a65822370
UberMensh0 <148571649+UberMensh0@users.noreply.github.com>
2023-11-23 02:15:56 +0400

    Album.java

--- kiu23wspt-kiu_gzobava.lasha/src/pgdp/Album.java
+++ kiu23wspt-kiu_gzobava.lasha/src/pgdp/Album.java
@@ -7,8 +7,8 @@ import java.util.Random;
 public class Album {
 
     //attributes
-    private  String title;
-    private  int releaseYear;
+    private String title;
+    private int releaseYear;
     private Song[] songs;
     private int numberOfSongs;
 
@@ -25,7 +25,9 @@ public class Album {
         return this.releaseYear;
     }
 
-    public Song[] getSongs() {return this.songs;}
+    public Song[] getSongs() {
+        return this.songs;
+    }
 
     public Album(String title, int releaseYear) {
         this.title = title;
@@ -33,96 +35,86 @@ public class Album {
     }
 
 
-//method to add songs
-int addSongs(Song[] songs){
-        if (this.songs == null){
+    //method to add songs
+    int addSongs(Song[] songs) {
+        if (this.songs == null) {
             this.songs = songs;
             return songs.length;
         }
         int count = 0;
         ArrayList<Song> songsList = new ArrayList<>(Arrays.asList(songs));
 
-    for (int i = 0; i < songs.length; i++) {
-        Song song = songs[i];
+        for (int i = 0; i < songs.length; i++) {
+            Song song = songs[i];
 
-        if (!songsList.contains(song)){
-            songsList.add(song);
-            count++;
+            if (!songsList.contains(song)) {
+                songsList.add(song);
+                count++;
+            }
         }
-    }
         this.songs = songsList.toArray(new Song[0]);
         return count;
 
-}
-
-//method to return songs in random order;
-Song[] shuffle() {
-    Song[] shuffledCopy = this.songs.clone();
-    Random random = new Random();
-
-    for (int i = shuffledCopy.length - 1; i > 0; i--) {
-        int x = random.nextInt(i + 1);
-
-        Song temp = shuffledCopy[x];
-        shuffledCopy[x] = shuffledCopy[i];
-        shuffledCopy[i] = temp;
     }
-    return shuffledCopy;
-}
 
-// This method returns the songs sorted by the lexicographical comparison of song title. And by descending order  if isAscending is false.
-// I use Selection Sort here
+    //method to return songs in random order;
+    Song[] shuffle() {
+        Song[] shuffledCopy = this.songs.clone();
+        Random random = new Random();
 
-    Song[] sortByTitle(boolean isAscending){
-        if (isAscending){
+        for (int i = shuffledCopy.length - 1; i > 0; i--) {
+            int x = random.nextInt(i + 1);
 
-            for (int i = 0; i < songs.length; i++) {
-                for (int j = i+1; j < songs.length; j++) {
-
-                    if (songs[i].toString().compareTo(songs[j].toString()) > 0){
-                        Song temp = songs[i];
-                        songs[i] = songs[j];
-                        songs[j] = temp;
-                    }
-                }
-            }
+            Song temp = shuffledCopy[x];
+            shuffledCopy[x] = shuffledCopy[i];
+            shuffledCopy[i] = temp;
         }
-        if (!isAscending){
-            for (int i = 0; i < songs.length; i++) {
-                for (int j = i+1; j < songs.length; j++) {
+        return shuffledCopy;
+    }
 
-                    if (songs[i].getTitle().compareTo(songs[j].getTitle()) < 0){
-                        Song temp = songs[i];
-                        songs[i] = songs[j];
-                        songs[j] = temp;
-                    }
-                }
-            }
-        }
-        return songs;}
-
-// This method returns the songs sorted by the comparison of song Durations.If isAscending is false, return the songs in descending order.
-// I use Bubble Sort here
-
-    Song[] sortByDuration(boolean isAscending){
+    // This method returns the songs sorted by the lexicographical comparison of song title. And by descending order  if isAscending is false.
+    // I use Selection Sort here
+    Song[] sortByTitle(boolean isAscending) {
         if (isAscending) {
+            selectionSort(songs);
+        }
+        if (!isAscending) {
+            selectionSort(songs);
+            reverseArray(songs);
+        }
+        return songs;
+    }
 
-        for (int i = 0; i < songs.length - 1; i++) {
-            for (int j = 0; j < songs.length - i - 1; j++) {
-                if (songs[j].getDuration() > songs[j + 1].getDuration()){
-                    Song temp = songs[j];
-                    songs[j] = songs[j + 1];
-                    songs[j + 1] = temp;
+    private static void selectionSort(Song[] songs){
+        for (int i = 0; i < songs.length; i++) {
+            for (int j = i + 1; j < songs.length; j++) {
 
+                if (songs[i].toString().compareTo(songs[j].toString()) > 0) {
+                    Song temp = songs[i];
+                    songs[i] = songs[j];
+                    songs[j] = temp;
                 }
             }
-          }
         }
+    }
 
+    // This method returns the songs sorted by the comparison of song Durations.If isAscending is false, return the songs in descending order.
+    // I use Bubble Sort here
+    Song[] sortByDuration(boolean isAscending) {
+        if (isAscending) {
+          bubbleSort(songs);
+        }
         if (!isAscending){
+            bubbleSort(songs);
+            reverseArray(songs);
+        }
+        return songs;
+    }
+
+    private static void bubbleSort(Song[] songs){
             for (int i = 0; i < songs.length - 1; i++) {
                 for (int j = 0; j < songs.length - i - 1; j++) {
-                    if (songs[j].getDuration() < songs[j + 1].getDuration()){
+                    if (songs[j].getDuration() > songs[j + 1].getDuration()) {
                         Song temp = songs[j];
                         songs[j] = songs[j + 1];
                         songs[j + 1] = temp;
@@ -130,56 +122,49 @@ Song[] shuffle() {
                     }
                 }
             }
+
+    }
+
+    // This method  returns the songs sorted by the comparison of song release years. if isAscending is false, return the songs in descending order.
+    // I use Insertion Sort here
+    Song[] sortByReleaseYear(boolean isAscending) {
+        if (isAscending) {
+            insertionSort(songs);
+        }
+        if (!isAscending) {
+            insertionSort(songs);
+            reverseArray(songs);
         }
         return songs;
     }
 
-// This method  returns the songs sorted by the comparison of song release years. if isAscending is false, return the songs in descending order.
-// I use Insertion Sort here
-      Song[] sortByReleaseYear(boolean isAscending){
-        if (isAscending){
-            for (int i = 1; i < songs.length; i++) {
-                Song temp = songs[i];
-                int j = i - 1 ;
-                while (j >= 0 && songs[j].getReleaseYear() > temp.getReleaseYear()){
-                    songs[j + 1] = songs[j];
-                    j--;
-                }
-                songs[j + 1] = temp;
-
+    private void insertionSort(Song[] songs2) {
+        for (int i = 1; i < songs.length; i++) {
+            Song temp = songs[i];
+            int j = i - 1;
+            while (j >= 0 && songs[j].getReleaseYear() > temp.getReleaseYear()) {
+                songs[j + 1] = songs[j];
+                j--;
             }
+            songs[j + 1] = temp;
+
+        }
+    }
+
+    // This method  returns the songs sorted by the comparison of popularity(likes). if isAscending is false, return the songs in descending order.
+    public Song[] sortByPopularity(boolean isAscending){
+        if (isAscending){
+            mergeSort(songs);
         }
         if (!isAscending){
-            for (int i = 1; i < songs.length; i++) {
-                Song temp = songs[i];
-                int j = i - 1 ;
-                while (j >= 0 && songs[j].getReleaseYear() < temp.getReleaseYear()){
-                    songs[j + 1] = songs[j];
-                    j--;
-                }
-                songs[j + 1] = temp;
-
-            }
+            mergeSort(songs);
+            reverseArray(songs);
         }
         return songs;
-      }
+    }
 
-// This method  returns the songs sorted by the comparison of popularity(likes). if isAscending is false, return the songs in descending order.
-
-Song[] sortByPopularity(boolean isAscending){
-        if (isAscending){
-mergeSort(songs);}
-        if (!isAscending){
-mergeSort(songs);
-reverseArray(songs);
-        }
-        return songs;
-
-
-}
-
-// Merge Sort algorithm
-private static void mergeSort(Song[] songs) {
+    // Merge Sort algorithm
+    private static void mergeSort (Song[]songs){
         int length = songs.length;
         if (length <= 1) return;
         int middle = length / 2;
@@ -197,39 +182,39 @@ private static void mergeSort(Song[] songs) {
         merge(leftArray, rightArray, songs);
     }
 
-private static void merge(Song[] leftArray, Song[] rightArray, Song[] songs){
+    private static void merge (Song[]leftArray, Song[]rightArray, Song[]songs){
         int leftLen = songs.length / 2;
         int rightLen = songs.length - leftLen;
         int i = 0; //for original array
         int l = 0; // for left array
         int r = 0; // for right array
 
-    while (l < leftLen && r < rightLen){
-        if (leftArray[l].getLikes() < rightArray[r].getLikes()){
+        while (l < leftLen && r < rightLen) {
+            if (leftArray[l].getLikes() < rightArray[r].getLikes()) {
+                songs[i] = leftArray[l];
+                i++;
+                l++;
+            } else {
+                songs[i] = rightArray[r];
+                i++;
+                r++;
+            }
+        }
+        while (l < leftLen) {
             songs[i] = leftArray[l];
             i++;
             l++;
         }
-        else {
+
+        while (r < rightLen) {
             songs[i] = rightArray[r];
             i++;
             r++;
         }
-    }
-    while (l < leftLen){
-        songs[i] = leftArray[l];
-        i++;
-        l++;
+
     }
 
-    while (r < rightLen){
-        songs[i] = rightArray[r];
-        i++;
-        r++;
-    }
-
-}
-    private static void reverseArray(Song[] songs) {
+    public static void reverseArray (Song[]songs){
         int left = 0;
         int right = songs.length - 1;
         while (left < right) {
